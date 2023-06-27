0860a7c11cba070a0988ec9ea8fbae9f16adf618
Paul Oloyede <52790286+paulayo93@users.noreply.github.com>
2023-06-27 11:41:12 +0100

    update map and restaurant

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,23 +1,16 @@
 import camelize from "camelize";
-
-import { locations } from "./location.mock";
+import { host } from "../../utils/env";
 
 export const locationRequest = (searchTerm) => {
-  return new Promise((resolve, reject) => {
-    const locationMock = locations[searchTerm];
-
-    if (!locationMock) {
-      reject("not found");
-    }
-    resolve(locationMock);
+  return fetch(`${host}/geocode?city=${searchTerm}`).then((res) => {
+    return res.json();
   });
 };
 
 export const locationTransform = (result) => {
   const formattedResponse = camelize(result);
-  const { geometry = {} } = formattedResponse?.results[0];
-  const { lat, lng } = geometry?.location;
+  const { geometry = {} } = formattedResponse.results[0];
+  const { lat, lng } = geometry.location;
 
-
-  return { lat, lng, viewport: geometry?.viewport };
+  return { lat, lng, viewport: geometry.viewport };
 };
